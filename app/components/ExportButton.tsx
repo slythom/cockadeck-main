f55044b75a3475429d1186e4f95fa4936618aeab
slythom <contact@sylvainthomas.fr>
2025-04-26 22:54:02 +0200

    Tweak: Traduction FR

--- app/components/ExportButton.tsx
+++ app/components/ExportButton.tsx
@@ -41,7 +41,7 @@ export default function ExportButton() {
       onClick={downloadDeck}
       className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
     >
-      Export Deck
+      Exporter le deck
     </button>
   );
 }
--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -5,10 +5,10 @@ export default async function Form() {
   return (
     <div>
       <form action={createDeck}>
-        <input required type="text" name="cardqty" id="cardqty" placeholder="Quantity*" className="outline p-2 m-2" />
-        <input required type="text" name="setcode" id="setcode" placeholder="Set Code*" className="outline p-2 m-2" />
-        <input required type="text" name="collectornumber" id="collectornumber" placeholder="Card Number*" className="outline p-2 m-2" />
-        <button type="submit" className="border-2 border-sky-500 p-2 m-2 cursor-pointer">Add to the deck</button>
+        <input required type="text" name="cardqty" id="cardqty" placeholder="Quantité*" className="outline p-2 m-2" />
+        <input required type="text" name="setcode" id="setcode" placeholder="Code du set*" className="outline p-2 m-2" />
+        <input required type="text" name="collectornumber" id="collectornumber" placeholder="Numéro de la carte*" className="outline p-2 m-2" />
+        <button type="submit" className="border-2 border-sky-500 p-2 m-2 cursor-pointer">Ajouter au deck</button>
       </form>
     </div>
   );
